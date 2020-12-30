83df4527d3f4ed9a8a4921da5c1dd5c55359dca4
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-12-30 12:00:13 -0800

    added comments to roll_dice in game_logic.py; filled out README; added comments and more tests for roll_dice to test...py

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -14,11 +14,15 @@ class GameLogic:
 
     @staticmethod
     def roll_dice(die_num = 6) -> tuple:
+        # create tuple with random int 1-6
+        # The length of tuple must match the argument given 
+        
         roll_result = tuple(())
-
-        for die in range(1, die_num + 1):
-            roll_result += (random.randint(1, 6),)
-        return roll_result
+        # loop through rolling single die for die_num number of times
+        # add to tuple and return 
+        for die in range(1,die_num+1):
+            roll_result = roll_result + (random.randint(1,6),)
+        return roll_result   
 
 
 class Banker:
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -3,13 +3,41 @@ roll_dice = GameLogic.roll_dice
 from game_of_greed import __version__
 
 
+# passes
 def test_version():
     assert __version__ == '0.1.0'
 
 
+### Testing - Roll Dice
+# When rolling 1 to 6 dice ensure…
 
-# sequence of correct length is returned
+# 1) A sequence of correct length is returned
+
+# passes: generates correct length
 def test_roll_dice_length():
     actual = len(roll_dice(4))
     expected = 4
     assert actual == expected
+    
+# 2) Each item in sequence is an integer with value between 1 and 6
+# passes: broken into separate verifications
+def test_roll_dice_int():
+    our_tuple = roll_dice(4)
+    result = 'types'
+    for item in range (0,4):
+        result = f'{result} : {type(our_tuple[item])}'
+    actual = result
+    expected = "types : <class 'int'> : <class 'int'> : <class 'int'> : <class 'int'>" 
+    assert actual == expected
+    
+def test_roll_dice_range():
+    our_second_tuple = roll_dice(4)
+    result = 'result:'
+    for item in range (0,4):
+        if 1 <= our_second_tuple[item] <= 6:
+            result = f'{result} Ok'
+            
+    actual = result
+    expected = "result: Ok Ok Ok Ok" 
+    assert actual == expected
+
